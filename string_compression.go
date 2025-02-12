4a4fbadf566a5cfcd721d860ce99091686498dc0
Jacob <jfoulds00@gmail.com>
2025-02-11 19:17:59 -0500

    final solution

--- string_compression.go
+++ string_compression.go
@@ -6,22 +6,21 @@ import (
 )
 
 func main() {
-	// chars := []byte("aabbccc")
-	chars := []byte("aaabbaa")
+	chars := []byte("aabbccc")
+	// chars := []byte("aaabbaa")
 	result_len := compress(chars)
 	fmt.Printf("The length of the compressed string is: %d\n", result_len)
-	fmt.Println(byte('b'))
 }
 
-func compress(chars []byte) int {
-	var last_char byte
+func compress_old(chars []byte) int {
+	var last_char byte = chars[0]
 	var start int
 	var end int
 	var compressed_str []byte
 	num_ch := 1
 	chars = append(chars, byte(0))
-	for i, ch := range chars {
-		if ch != last_char {
+	for i := 1; i < len(chars); i++ {
+		if chars[i] != last_char {
 			switch num_ch {
 			case 1:
 				// Do nothing because non-repeated char cannot be compressed with this algorithm
@@ -38,9 +37,42 @@ func compress(chars []byte) int {
 		} else {
 			num_ch++
 		}
-		last_char = ch
+		last_char = chars[i]
 	}
 	chars = chars[:len(chars)-1]
 	fmt.Println(string(chars))
 	return len(chars)
+}
+
+func compressSlice(start int, end int, chars []byte, c byte) {
+	for j := start; j < end; j++ {
+
+	}
+}
+
+func compress(chars []byte) int {
+	write := 0
+	read := 0
+	
+	for read < len(chars) {
+		char := chars[read]
+		count := 0
+
+		for read < len(chars) && chars[read] == char {
+			read++
+			count++
+		}
+
+		chars[write] = char
+		write++
+		
+		if count > 1 {
+			for _, digit_char := range strconv.Itoa(count){
+				chars[write] = byte(digit_char)
+				write++
+			}
+		}
+	}
+	fmt.Println(string(chars))
+	return write
 }
